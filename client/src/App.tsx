c84f8bf7a6124459d66d85d5a64bb22ef032429a
jelanifrancis@spelman.edu <jelanifrancis@Jelanis-MacBook-Pro.local>
2025-04-23 13:50:20 -0400

    fix route paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,10 @@ import PythonConditionals from './pages/tutorials/PythonConditionals';
 import JavaScriptVariables from './pages/tutorials/JavaScriptVariables';
 import JavaScriptFunctions from './pages/tutorials/JavaScriptFunctions';
 import JavaScriptConditionals from './pages/tutorials/JavaScriptConditionals';
+import JavaScriptArrays from './pages/tutorials/JavaScriptArrays';
+import JavaScriptLoops from './pages/tutorials/JavaScriptLoops';
+import PythonLists from './pages/tutorials/PythonLists';
+import PythonLoops from './pages/tutorials/PythonLoops';
 import { LanguageContext, useLanguageProvider } from './hooks/useLanguage';
 
 function Router() {
@@ -25,6 +29,10 @@ function Router() {
       <Route path="/tutorials/javascript/variables" component={JavaScriptVariables} />
       <Route path="/tutorials/javascript/functions" component={JavaScriptFunctions} />
       <Route path="/tutorials/javascript/conditionals" component={JavaScriptConditionals} />
+      <Route path="/tutorials/javascript/arrays" component={JavaScriptArrays} />
+      <Route path="/tutorials/javascript/loops" component={JavaScriptLoops} />
+      <Route path="/tutorials/python/lists" component={PythonLists} />
+      <Route path="/tutorials/python/loops" component={PythonLoops} />
       <Route component={NotFound} />
     </Switch>
   );
